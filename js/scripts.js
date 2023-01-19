4e009a231839094063cde25adb9346dd09b67624
Reuben Ngururu <RNGURURU@safaricom.co.ke>
2023-01-19 12:30:24 +0300

    write code to create a form

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,14 @@
 //     alert("here");
 //   })
 // })
-
-
-
-
 $(document).ready(function(){
     $("#development-image").click(function(){
-      $("#development-image").slideDown('1500').hide('1000');
-      $("#development").show('1500');
+      $("#development-image").hide();
+      $("#development").show();
     });
     $("#development").click(function(){
-      $("#development").slideUp('1500');
-      $("#development-image").slideDown('1500');
+      $("#development").slideUp();
+      $("#development-image").slideDown();
     });
   });
   
